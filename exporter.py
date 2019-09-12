48bcf2f55f2260f896f865dcbca164f7439bf3df
jasmin <jsmincbf125@gmail.com>
2019-09-13 02:55:06 +0900

    initial commit

--- exporter.py
+++ exporter.py
@@ -1,3 +1,13 @@
-import docker_pid as dp
+import docker
 
-print(dp.docker_pids())
+client=docker.from_env()
+
+containers = client.containers.list()
+
+for container in containers:
+    try:
+        processes=container.top(ps_args="aux")
+        print(processes["Processes"])
+    except docker.errors.APIError:
+        pass
+print(containers)
