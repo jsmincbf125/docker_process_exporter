c3bf8abf1753d9a6d7a661096dd6eca68ab4a0e5
Kazuaki Nomura <nomura@m.cs.osakafu-u.ac.jp>
2019-09-30 23:33:09 +0900

    Added exporter.go

---
+++ exporter.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+func main() {
+	cli, err := client.NewClientWithOpts(client.WithVersion("1.39"))
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		process, _ := cli.ContainerTop(context.Background(), container.ID, []string{""})
+		fmt.Print(process)
+	}
+}
